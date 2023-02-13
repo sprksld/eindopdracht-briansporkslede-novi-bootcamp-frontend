8750b49f0c11212b80e42bad3589d474caa20249
sprksld <b.sporkslede@xs4all.nl>
2023-02-13 12:53:32 +0100

    footer and home in place

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
       <>
           It Works!
+          <Routes>
+              <Route exact path="/" element={<Home/>} />
+          </Routes>
       </>
   );
 }
